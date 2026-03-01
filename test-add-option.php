9f8527cca529d0651c06b0f4327137d6121c6d17
sajjad-amin <sayem.1998.bd@gmail.com>
2026-03-01 20:36:18 +0600

    Remove debug scripts

--- test-add-option.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('../../../wp-load.php');
-delete_option('wpadt_test_false');
-$res = add_option('wpadt_test_false', false);
-var_dump($res);
-$val = get_option('wpadt_test_false', 'DEFAULT');
-var_dump($val);
